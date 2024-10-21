0a5097802d4491e8f35f881176ec17e670403bb5
izabellics <110102381+izabellics@users.noreply.github.com>
2024-10-21 08:10:38 -0300

    21/10

--- ciencia de dados/graficos/graficosGlobais.js
+++ ciencia de dados/graficos/graficosGlobais.js
@@ -1,13 +1,9 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json";
 
 async function visualizarInformacoesGlobais() {
+    const res = await fetch(url);
+    const dados = res.json()
+    console.log(dados);
+}
 
-        const res = await fetch(url)
-
-        const dados = await res.json()
-
-        console.log(dados);
-
-    }
-
-    visualizarInformacoesGlobais()
+visualizarInformacoesGlobais();
